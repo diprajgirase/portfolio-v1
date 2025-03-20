91d82581b08ca81521567b9c0e0414273ce781a9
Dipraj Girase <girasedipraj52@gmail.com>
2025-03-20 22:54:57 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -13,12 +13,12 @@ function Hero() {
       id="hero"
       className="mx-auto mb-28 max-w-3xl px-4 flex flex-col items-center justify-center text-center"
     >
-<h1 className="mb-8 flex items-center justify-center gap-2 text-[clamp(2.5rem,5vw,6rem)] whitespace-nowrap overflow-hidden">
+<h1 className="mb-8 flex items-center justify-center gap-4 text-[clamp(2.5rem,5vw,6rem)] whitespace-nowrap overflow-hidden">
   <span className="font-medium tracking-normal">Design</span>
   <span className="font-medium tracking-normal">Develop</span>
   <span className="font-medium tracking-normal">Deploy</span>
 </h1>
-      <p className="mb-10 w-full max-w-[90%] text-lg text-slate-600 dark:text-slate-300 md:text-xl">
+      <p className="mb-8 w-full max-w-[90%] text-lg text-slate-600 dark:text-slate-300 md:text-xl">
         I design and develop web applications and deploy them in weeks, not months.
         Building applications that people can use.
       </p>
